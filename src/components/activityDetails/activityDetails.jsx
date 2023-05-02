6475f0850914f2c72a6ba7cb45fe595a128581d8
Charlese <charles.easton123@gmail.com>
2023-05-02 02:57:16 -0400

    add number of miss call icon

--- src/components/activityDetails/activityDetails.jsx
+++ src/components/activityDetails/activityDetails.jsx
@@ -9,9 +9,12 @@ function activityDetails() {
   //   name,
   //   date,
   //   time,
+  //   numberofCalls,
   // ]
 
   return (
+    <div className="time-container">
+      <span className="date"> July 27, 2017</span>
     <div className="details-container">
       <div className="content">
         <span>
@@ -20,16 +23,20 @@ function activityDetails() {
         </span>
 
         <div className="number-call">
+          <div>
           <span className="number">+336 45 13 53 91</span>
+          <span className="red-circle"> 5</span>
+          </div>
           <span>tried to call on </span>
         </div>
 
         <div className="time">
-          <span className="number">7: 58</span>
+          <span>7: 58</span>
           <span> PM </span>
         </div>
 
       </div>
+    </div>
     </div>
   );
 }
